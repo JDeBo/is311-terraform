542f78cdcf3bf53defb59a181253a59ca2ab4f3b
Justin <justin.debo@gmail.com>
2022-02-24 13:17:01 -0600

    creating EC2 lab basics

--- labs/ec2/main.tf
+++ labs/ec2/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-remote-state-932196253170"
-    key    = "is-311"
+    key    = "is-311-ec2-in-class"
     region = "us-east-2"
   }
 }
@@ -10,21 +10,65 @@ provider "aws" {
   region = "us-east-2"
 }
 
-module "student_iam" {
-  source = "./modules/student_user"
+data "aws_ami" "linux_2" {
+  most_recent = true
 
-  for_each            = var.students
-  student_name        = each.value.name
-  student_resource_id = each.key
-  keybase_id          = each.value.keybase_id
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm-2*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["137112412989"] # AWS
 }
 
-module "teacher_iam" {
-  source = "./modules/teacher_user"
+resource "aws_instance" "lab" {
+  count         = var.instance_count
+  ami           = data.aws_ami.linux_2.id
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.key_pair.id
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
-  for_each            = var.teachers
-  teacher_name        = each.value.name
-  teacher_resource_id = each.key
-  keybase_id          = each.value.keybase_id
-  class_id            = var.class_id
+  tags = {
+    Name = "Group-${count.index+1}"
+  }
+
+  depends_on = [
+    aws_security_group.allow_ssh
+  ]
+}
+
+resource "aws_key_pair" "key_pair" {
+  key_name   = "terraform_key"
+  public_key = var.rsa_public_key
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
 }
--- labs/ec2/variables.tf
+++ labs/ec2/variables.tf
@@ -1,14 +1,12 @@
-variable "students" {
-  description = "Map of students to create users for."
-  type        = map(map(string))
-  default = { "justin-debo" = {
-    "name"       = "Justin DeBo"
-    "email"      = "jdebo@millikin.edu"
-    "keybase_id" = "jdebomillikin"
-  } }
+variable "rsa_public_key" {
+  description = "Public key used to create key pair"
 }
 
-variable "class_id" {
-  description = "ID for the class that is being taught"
-  default     = ""
+variable "instance_count" {
+  description = "Number of EC2 instances to create for lab"
+  default     = 1
+}
+
+variable "vpc_id" {
+  description = "The VPC ID for security group to connect to"
 }
