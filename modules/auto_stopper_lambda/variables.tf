13c82019216ccf7cb98595f43b05bae8d92f9551
Justin <justin.debo@gmail.com>
2022-04-24 11:30:09 -0500

    removing another unused variable

--- modules/auto_stopper_lambda/variables.tf
+++ modules/auto_stopper_lambda/variables.tf
@@ -1,7 +1,3 @@
-variable "rsa_public_key" {
-  description = "Public key used to create key pair"
-}
-
 variable "use_case" {
   description = "Unique identifier for the use case the EC2 instances are created for. (Must be CamelCase)"
 }
