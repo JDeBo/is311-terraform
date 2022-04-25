f5dfea464cdd55197422ca06dc749da8dfd88bae
Justin <justin.debo@gmail.com>
2022-04-25 11:44:53 -0500

    adding public IPs to networking lab

--- labs/networking/student_resources/subnet.tf
+++ labs/networking/student_resources/subnet.tf
@@ -2,6 +2,7 @@ resource "aws_subnet" "this" {
   vpc_id            = var.vpc_id
   cidr_block        = var.subnet_cidr
   availability_zone = "us-east-2a"
+  map_public_ip_on_launch = true
 
   tags = {
     Name  = "is311-networking-${var.student_id}"
