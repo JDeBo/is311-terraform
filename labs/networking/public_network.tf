a892e847de949cc6d5c54a4084b91b6afd5f71b0
Justin <justin.debo@gmail.com>
2022-04-25 12:21:43 -0500

    adding public instance to lab

--- labs/networking/public_network.tf
+++ labs/networking/public_network.tf
@@ -25,6 +25,7 @@ resource "aws_subnet" "this" {
   vpc_id = aws_vpc.networking_lab.id
   cidr_block        = "172.32.12.0/24"
   availability_zone = "us-east-2a"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "Lab Public Subnet"
@@ -48,4 +49,29 @@ resource "aws_route_table" "this" {
 resource "aws_route_table_association" "this" {
   subnet_id      = aws_subnet.this.id
   route_table_id = aws_route_table.this.id
+}
+
+module "ec2" {
+  source = "./../../modules/ec2_instance"
+  name = "is311-networking-public-instance"
+  subnet_id = aws_subnet.this.id
+  vpc_security_group_list = [aws_security_group.this.id]
+}
+
+resource "aws_security_group" "this" {
+  name        = "is311-networking-public-instance"
+  description = "Networking Lab public instance subnet"
+  vpc_id      = aws_vpc.networking_lab.id
+
+  ingress {
+    description = "SSH from Anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "is311-networking-public-instance"
+  }
 }
