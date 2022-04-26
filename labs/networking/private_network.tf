a0887f07decbffc4a8111f0a999a7fd1b09d1489
Justin <justin.debo@gmail.com>
2022-04-25 21:30:58 -0500

    ugh

--- labs/networking/private_network.tf
+++ labs/networking/private_network.tf
@@ -113,7 +113,7 @@ resource "aws_security_group" "this" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = 0.0.0.0/0
+    cidr_blocks = "0.0.0.0/0"
   }
   
   tags = {
