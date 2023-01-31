4dd297b362b683828c778d1c8e9b8d67f119b309
Justin <justin.debo@gmail.com>
2023-01-30 19:36:16 -0600

    Public Subnet1a

--- labs/ec2-individual/main.tf
+++ labs/ec2-individual/main.tf
@@ -30,7 +30,7 @@ data "aws_vpc" "controltower" {
 data "aws_subnet" "controltower" {
   filter {
     name   = "tag:Name"
-    values = ["PublicSubnet1*"] # update for you subnets
+    values = ["PublicSubnet1a"] # update for you subnets
   }
 }
 
