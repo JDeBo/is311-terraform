208dab95dd4452f812d28fe5a1487d5d2c545bce
Justin <justin.debo@gmail.com>
2023-02-16 19:36:30 -0600

    .

--- main/sso/policies.tf
+++ main/sso/policies.tf
@@ -72,11 +72,11 @@ resource "aws_iam_policy" "students" {
   policy      = data.aws_iam_policy_document.sso.json
 }
 
-resource "aws_ssoadmin_customer_managed_policy_attachment" "students" {
-  instance_arn       = local.sso_instance_arn
-  permission_set_arn = aws_ssoadmin_permission_set.students.arn
-  customer_managed_policy_reference {
-    name = aws_iam_policy.students.name
-    path = "/"
-  }
-}
+# resource "aws_ssoadmin_customer_managed_policy_attachment" "students" {
+#   instance_arn       = local.sso_instance_arn
+#   permission_set_arn = aws_ssoadmin_permission_set.students.arn
+#   customer_managed_policy_reference {
+#     name = aws_iam_policy.students.name
+#     path = "/"
+#   }
+# }
